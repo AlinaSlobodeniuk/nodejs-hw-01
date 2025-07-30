27375c76193b887c155b355f32b03f012878c597
Alina Slobodeniuk <112395038+AlinaSlobodeniuk@users.noreply.github.com>
2025-07-30 03:58:03 +0300

    done

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+    try {
+        const data = JSON.stringify(updatedContacts, null, 2);
+        await fs.writeFile(PATH_DB, data, 'utf8' );
+return;
+    } catch (error) {
+        console.error(error);
+        return;
+    }
+};
